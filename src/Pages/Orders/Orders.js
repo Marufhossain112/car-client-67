506080299063ac5da8ae0a923a71982c924b65d7
maruf <dev.maruf112@gmail.com>
2022-11-04 08:24:36 +0600

    tabular format is done

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import OrdersRow from "./OrdersRow";
 
 const Orders = () => {
-  const [orders, setOrders] = useState({});
+  const [orders, setOrders] = useState([]);
   const { user } = useContext(AuthContext);
   useEffect(() => {
     fetch(`http://localhost:5000/orders?email=${user?.email}`)
@@ -11,7 +12,26 @@ const Orders = () => {
   }, [user?.email]);
   return (
     <div>
-      <h2>This is order page for {orders.length}</h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>
+              {/*  */}
+              </th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <OrdersRow key={order._id} order={order}></OrdersRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
